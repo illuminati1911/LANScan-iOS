db367f29e9880b0cd039056bca4dfb70b6620f2d
Ville Välimaa <ville.valimaa@futurice.com>
2017-01-23 21:58:29 +0200

    - Added images and models

---
+++ LANScan/Host.swift
@@ -0,0 +1,15 @@
+//
+//  Host.swift
+//  TestApp
+//
+//  Created by Ville Välimaa on 19/01/2017.
+//  Copyright © 2017 Ville Välimaa. All rights reserved.
+//
+
+
+struct Host {
+    let ipAddress:String?
+    let hostname:String?
+    let macAddress:String?
+    var manufacturer:String?
+}
