bdd24d30dfa95f2fcb8f11ef8d9b44b5b4bf0ef6
Ville Välimaa <ville.valimaa@futurice.com>
2017-01-23 22:06:16 +0200

    - Added dependencies to bridging header

--- LANScan/LANScan-Bridging-Header.h
+++ LANScan/LANScan-Bridging-Header.h
@@ -2,3 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "SimplePing.h"
+#import "Reachability.h"
+#import "HostnameResolver.h"
+#import "MacFinder.h"
+#import <SpinKit/RTSpinKitView.h>
+#include <ifaddrs.h>
